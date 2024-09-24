4e3630df78c7210755876e851a0d96291e284705
Kareena Patel <kareenapat17@gmail.com>
2024-09-24 20:56:53 +0100

    Implement useContext + Import ColoursList component

--- src/components/ColourInputer.tsx
+++ src/components/ColourInputer.tsx
@@ -1,42 +1,43 @@
-import { useState } from "react";
-import ColourBlock from "./ColourBlock";
+import { createContext, useState } from "react";
+import ColoursList from "./ColoursList";
 import { v4 as uuid } from "uuid"
 import "../styles/ColourInputer.css"
+import { Colour } from "../reusableTypes/colourType"
 
-// Type alias used throughout
-type Colour = {
-    id: String,
-    hexColour: String
+export type RemoveFromListContextType = {
+    removeFromList: (value: String) => void
 }
+
+export const RemoveFromListContext = createContext<RemoveFromListContextType | null>(null)
 
 export default function ColourInputer() {
     const [enteredColour, setEnteredColour] = useState("")
 
     // Colour type alias assigned as used in List to store item objects
-    const [colourList, setColourList] = useState<Colour[]>([])    
+    const [colourList, setColourList] = useState<Colour[]>([])
 
-    const handleSubmit = (e: { preventDefault: () => void; }):void => {
+    const handleSubmit = (e: { preventDefault: () => void; }): void => {
         e.preventDefault();
         { validation() ? addColour() : null }
         setEnteredColour('') // Clears input after submission 
     }
 
     // Delete item from array in state
-    const removeFromList = (value: String):void => {
+    const removeFromList = (value: String): void => {
         setColourList(colourList.filter(c => c.id !== value))
     }
 
     // Validate input before adding to list
-    const validation = ():Boolean => {
+    const validation = (): Boolean => {
         const regex = /^#?([a-f0-9]{6}|[a-f0-9]{3})$/i; // Regex expression to validate string
         if (enteredColour.match(regex)) return true
         else return false
     }
 
     // Appending to an array in state
-    const addColour = ():void => {
+    const addColour = (): void => {
         // id added to help with deleting item + making component unique to siblings
-        const newColour:Colour = {
+        const newColour: Colour = {
             id: uuid(),
             hexColour: `#${enteredColour.toUpperCase()}`
         }
@@ -57,17 +58,9 @@ export default function ColourInputer() {
                 <input type="submit" value="Add" onClick={validation} />
             </form>
 
-            <ul>
-            {colourList.map(item =>
-                    <li><ColourBlock
-                        key ={uuid()}
-                        id ={item.id} // Can be accessed in code
-                        colour={item.hexColour}
-
-                        // Delete function passed into component
-                        remove={removeFromList}
-                    /></li>)}
-            </ul>
+            <RemoveFromListContext.Provider value={{ removeFromList }}>
+                <ColoursList colourList={colourList}/>
+            </RemoveFromListContext.Provider>
         </>
     )
 }
