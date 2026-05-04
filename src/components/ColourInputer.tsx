60405cc80922ff11192bc110d25817a56be10db4
Kareena Patel <kareenapat17@gmail.com>
2026-05-04 21:26:01 +0100

    Change how uuid is created

--- src/components/ColourInputer.tsx
+++ src/components/ColourInputer.tsx
@@ -1,77 +1,76 @@
 import { createContext, useCallback, useEffect, useMemo, useState } from "react";
 import ColoursList from "./ColoursList";
-import { v4 as uuid } from "uuid"
-import "../styles/ColourInputer.css"
-import { Colour } from "../reusableTypes/colourType"
+import "../styles/ColourInputer.css";
+import { Colour } from "../reusableTypes/colourType";
 import useDebounce from "../hooks/useDebounce";
 
 export type RemoveFromListContextType = {
-    removeFromList: (value: String) => void
-}
+    removeFromList: (value: String) => void;
+};
 
-export const RemoveFromListContext = createContext<RemoveFromListContextType | null>(null)
+export const RemoveFromListContext = createContext<RemoveFromListContextType | null>(null);
 
 export default function ColourInputer() {
-    const [enteredColour, setEnteredColour] = useState("")
-    const [colourWord, setColourWord] = useState("")
-    const [colourList, setColourList] = useState<Colour[]>([])
-    const [invalidColourEntered, setInvalidColourEntered] = useState(false)
-    const [hint, setHint] = useState(false)
+    const [enteredColour, setEnteredColour] = useState("");
+    const [colourWord, setColourWord] = useState("");
+    const [colourList, setColourList] = useState<Colour[]>([]);
+    const [invalidColourEntered, setInvalidColourEntered] = useState(false);
+    const [hint, setHint] = useState(false);
 
     // Custom hook used to reduce unnecessary API calls 
-    const debouncedColour = useDebounce(enteredColour, 200)
+    const debouncedColour = useDebounce(enteredColour, 200);
 
     useEffect(() => {
         async function getColourWord() {
-            if (enteredColour === "") return null
+            if (enteredColour === "") return null;
             try {
-                const response = await fetch(`https://www.thecolorapi.com/id?hex=${debouncedColour}&format=json`)
-                const data = await response.json()
-                setColourWord(data.name.value)
+                const response = await fetch(`https://www.thecolorapi.com/id?hex=${debouncedColour}&format=json`);
+                const data = await response.json();
+                setColourWord(data.name.value);
             }
             catch (err) {
-                console.error(err)
+                console.error(err);
             }
         }
-        getColourWord()
-    }, [debouncedColour])
+        getColourWord();
+    }, [debouncedColour]);
 
     const handleSubmit = (e: { preventDefault: () => void; }): void => {
         e.preventDefault();
-        { validation() ? addColour() : setInvalidColourEntered(true) }
-    }
+        { validation() ? addColour() : setInvalidColourEntered(true); }
+    };
 
     const addColour = (): void => {
-        setHint(true)
+        setHint(true);
         const newColour: Colour = {
-            id: uuid(),
+            id: crypto.randomUUID(),
             hexColour: `#${enteredColour.toUpperCase()}`,
             colourName: ''
-        }
+        };
 
         if (colourWord) {
-            setHint(false)
-            setColourList([{ ...newColour, colourName: colourWord.toUpperCase() }, ...colourList])
-            setInvalidColourEntered(false)
-            setEnteredColour("")
+            setHint(false);
+            setColourList([{ ...newColour, colourName: colourWord.toUpperCase() }, ...colourList]);
+            setInvalidColourEntered(false);
+            setEnteredColour("");
         }
-        setColourWord('')
-    }
+        setColourWord('');
+    };
 
     // Delete item from array in state
     // Practice (not necessary) using useCallback to stop rerendering function in child
     const removeFromList = useCallback((value: String): void => {
-        setColourList(colourList.filter(c => c.id !== value))
-    }, [colourList])
+        setColourList(colourList.filter(c => c.id !== value));
+    }, [colourList]);
 
     const validation = (): Boolean => {
         const regex = /^#?([a-f0-9]{6}|[a-f0-9]{3})$/i;
-        if (enteredColour.match(regex)) return true
-        else return false
-    }
+        if (enteredColour.match(regex)) return true;
+        else return false;
+    };
 
     // CreateContext causes rerendering (when passing in a function) so useMemo is applied
-    const removeFromListProvider = useMemo(() => ({ removeFromList }), [colourList])
+    const removeFromListProvider = useMemo(() => ({ removeFromList }), [colourList]);
 
     return (
         <main>
@@ -99,5 +98,5 @@ export default function ColourInputer() {
                 <ColoursList colourList={colourList} />
             </RemoveFromListContext.Provider>
         </main>
-    )
+    );
 }
